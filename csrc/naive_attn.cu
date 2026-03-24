d7ec09c6366afd5414176ad03aec7d3b430c910f
John Wallace <john.wallace08@gmail.com>
2026-03-24 14:20:16 +0000

    Add naive attention implementation in CUDA

---
+++ csrc/naive_attn.cu
@@ -0,0 +1,81 @@
+#include "cublas_v2.h"
+
+
+__global__ void softmax(float *M, int N) {
+    int row = blockIdx.x;
+    int tid = threadIdx.x;
+
+    extern __shared__ float smem[];
+
+
+    // First, find row-max
+    float local_max = -FLT_MAX;
+    for(int i = tid; i < N; i += blockDim.x) {
+        local_max = fmaxf(local_max, M[row*N + i]);
+    }
+
+    smem[tid] = local_max;
+    __syncthreads();
+
+
+    for(int stride = blockDim.x / 2; stride > 0; stride >>= 1) {
+        if (tid < stride) {
+            smem[tid] = fmaxf(smem[tid], smem[tid + stride]);
+        }
+        __syncthreads();
+    }
+
+    float row_max = smem[0];
+
+
+    // Next, calculate element-wise exponents and keep track of sum
+    float local_sum = 0.0f;
+    for(int i = tid; i < N; i += blockDim.x) {
+        M[row*N + i] = expf(M[row*N + i] - row_max);
+        local_sum += M[row*N + i];
+    }
+
+    smem[tid] = local_sum;
+    __syncthreads();
+
+
+    for(int stride = blockDim.x / 2; stride > 0; stride >>= 1) {
+        if (tid < stride) {
+            smem[tid] += smem[tid + stride];
+        }
+        __syncthreads();
+    }
+
+    float row_sum = smem[0];
+
+
+    // Finally, normalize by the row sum
+    for(int i = tid; i < N; i += blockDim.x) {
+        M[row*N + i] /= row_sum;
+    }
+
+
+}
+
+
+void naive_attn(float *Q, float *K, float *V, float *O, int N, int d) {
+    cublasHandle_t handle;
+    cublasCreate(&handle);
+
+    // intermediate output matrix
+    float *C;
+    cudaMalloc(&C, N*N*sizeof(float));
+
+    float alpha = 1.0f/sqrtf((float) d);
+    float beta = 0.0f;
+
+    cublasSgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, N, N, d, &alpha, K, d, Q, d, &beta, C, N);
+    softmax<<<N, 256, 256*sizeof(float)>>>(C, N);
+
+    alpha = 1.0f;
+    beta = 0.0f;
+    cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, d, N, N, &alpha, V, d, C, N, &beta, O, d);
+
+    cudaFree(C);
+    cublasDestroy(handle);
+}
