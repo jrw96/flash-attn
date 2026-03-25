80d828c3af6cb8209699b89a7f16a383db507a0a
John Wallace <john.wallace08@gmail.com>
2026-03-25 17:26:48 +0000

    Add correctness tests for v2

--- tests/test_correctness.py
+++ tests/test_correctness.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn.functional as F
 import naive_attn
 import flash_attn
+import flash_attn_v2
 
 
 def test(label, O_custom, O_ref, tol=1e-3):
@@ -31,8 +32,13 @@ def run_tests():
             O_flash = flash_attn.flash_attn(Q, K, V)
             test(f"flash  N={N:4d} d={d}", O_flash, O_ref)
 
+            # Flash kernel v2
+            O_flash_v2 = flash_attn_v2.flash_attn_v2(Q, K, V)
+            test(f"flash  N={N:4d} d={d}", O_flash_v2, O_ref)
+
             # Naive vs flash directly
             test(f"naive==flash N={N:4d} d={d}", O_naive, O_flash)
+            test(f"naive==flash_v2 N={N:4d} d={d}", O_naive, O_flash_v2)
 
 
 if __name__ == "__main__":
